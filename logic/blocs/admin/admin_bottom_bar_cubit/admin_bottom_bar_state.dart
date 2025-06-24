17e9340059e651448a761c647139f07da75faad0
Lakshya2029 <157345982+Lakshya2029@users.noreply.github.com>
2025-06-24 16:17:40 +0530

    Delete logic/blocs directory

--- logic/blocs/admin/admin_bottom_bar_cubit/admin_bottom_bar_state.dart
+++
@@ -1,9 +0,0 @@
-part of 'admin_bottom_bar_cubit.dart';
-
-class AdminBottomBarState extends Equatable {
-  final int index;
-  const AdminBottomBarState(this.index);
-
-  @override
-  List<Object> get props => [index];
-}
